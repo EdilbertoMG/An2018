949295a471e167f6fb823749579e1b19672ab8b2
edilbertomgutierrez <edilbertomarrugo03@hotmail.com>
2019-01-24 16:24:05 -0500

    Curso de Angular / Fundamentos - Directiva ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my-app';
   show:boolean = true;
+  //arreglo tipo any de objetos que se inician con llaves: {}
+  animales:Array<any>=[
+    {tipo:'Perro',nombre:'Lisa',edad:20},
+    {tipo:'Gato',nombre:'Jese',edad:4},
+    {tipo:'Humano',nombre:'Edilberto',edad:22}
+  ];
 }
