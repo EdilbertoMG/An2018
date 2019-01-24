26581845c45126106e64af0e62718da5cbc12b48
edilbertomgutierrez <edilbertomarrugo03@hotmail.com>
2019-01-24 17:24:14 -0500

    Curso de Angular / Fundamentos - Configurando Rutas - RouterLink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,15 @@ import { CabeceraComponent } from './cabecera/cabecera.component';
 import { FooterComponent } from './footer/footer.component';
 import { CuerpoComponent } from './cuerpo/cuerpo.component';
 import { ContactoComponent } from './contacto/contacto.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
 
 //luego creamos la costante de las rutas dinamicas ponemos el nombre de la ruta y el componente que va a usar
 const routes: Routes = [
-  { path: 'contacto', component: ContactoComponent }
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: '', component: CuerpoComponent, pathMatch: 'full' },
+  //cuando la ruta no exista redirige a inicio en todos los casos
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
 ];
 
 @NgModule({
@@ -22,11 +27,13 @@ const routes: Routes = [
     CabeceraComponent,
     FooterComponent,
     CuerpoComponent,
-    ContactoComponent
+    ContactoComponent,
+    NosotrosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    //importamos las rutas
     RouterModule.forRoot(routes) 
   ],
   providers: [],
